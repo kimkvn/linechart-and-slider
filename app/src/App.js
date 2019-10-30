440ca491ffaa640ad3d51656e38a6ee9eeffb376
kimkvn <kimkvn@gmail.com>
2019-10-29 20:27:26 -0400

    jk, had to add some height and width to the chart div container

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo, { ReactComponent } from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 import './line-graph.scss';
@@ -33,21 +33,6 @@ class App extends React.Component {
         <div className="test">test
           <svg id="isa-modeled-performance-chart"></svg>
         </div>
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
